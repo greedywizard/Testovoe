52eebcf1976577691bd15fcb1c5e970c69619539
Greedy <22398454+greedywizard@users.noreply.github.com>
2023-05-21 05:05:05 +0300

    update

---
+++ Automizer/Actions/__init__.py
@@ -0,0 +1,7 @@
+from .Click import *
+from .GetElement import *
+from .GetShadowRoot import *
+from .Input import *
+from .OpenUrl import *
+from .Selected import *
+from .WaitElementVisible import *
